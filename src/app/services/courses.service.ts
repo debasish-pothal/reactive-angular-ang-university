9df5e95e46c91fcd48bdb6219491bf7469de8da5
Debasish Pothal <debasish_pothal@hotmail.com>
2023-07-25 06:07:38 +0530

    added search

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,14 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { Course } from '../model/course';
-import { map, shareReplay } from 'rxjs/operators';
+import { catchError, map, shareReplay } from 'rxjs/operators';
+import { Lesson } from '../model/lesson';
+import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/messages.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private loadingService: LoadingService,
+    private messagesService: MessagesService
+  ) {}
 
   loadAllCourses(): Observable<Course[]> {
     return this.http.get<Course[]>('/api/courses').pipe(
@@ -16,9 +23,29 @@ export class CoursesService {
       shareReplay()
     );
   }
+
   saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
     return this.http
       .put(`/api/courses/${courseId}`, changes)
       .pipe(shareReplay());
+  }
+
+  searchLessions(search: string): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>('/api/lessions', {
+        params: {
+          filter: search,
+          pageSize: '100',
+        },
+      })
+      .pipe(
+        catchError((err) => {
+          const message = 'Could not load lessions';
+          this.messagesService.showErrors(message);
+          return throwError(err);
+        }),
+        map((res) => res['payload']),
+        shareReplay()
+      );
   }
 }
